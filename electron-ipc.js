364151dc87dfe8ed9a33bcb3043764bdeab157cf
HoWingYip <sploooosh884@gmail.com>
2018-11-29 14:56:24 +0800

    Beginning preliminary work on displaying assets on import

--- electron-ipc.js
+++ electron-ipc.js
@@ -1,14 +1,21 @@
 const {ipcRenderer} = require("electron");
 
+var importedFiles = new Array();
+
 function importAssets() {
   ipcRenderer.send("importAssets");
 }
 
-ipcRenderer.on("importedAssetsSend", (_, importedFiles) => {
-  console.log(importedFiles);
+ipcRenderer.on("importedAssetsSend", (_, newlyImportedFiles) => {
+  console.log(`Newly imported files: \n${newlyImportedFiles}\n\n`);
+  for(var filename of newlyImportedFiles) {
+    importedFiles.push(filename);
+  }
+  console.log(`importedFiles array: \n${importedFiles}\n\n`);
   //hide "no files imported" placeholder if files were actually imported
   if(importedFiles.length > 0) {
     document.getElementsByClassName("placeholder-fullheight")[0].style.display = "none";
+    document.getElementById("assets").style.display = "table";
   }
 });
 
