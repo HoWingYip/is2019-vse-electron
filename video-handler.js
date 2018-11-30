6c3b8b3b2cddf8125ee4bbdeaaf1404713ce6a74
HoWingYip <sploooosh884@gmail.com>
2018-11-30 19:52:41 +0800

    Deleted unused code with old ffmpeg

--- video-handler.js
+++ video-handler.js
@@ -81,27 +81,3 @@ function extractThumbnails() {
   }
 }
 
-/*
-var sourceVideoMetadata;
-var timelineVideoMetadata;
-
-try {
-  var videoProcess = new ffmpeg('assets/PostingLetter.MOV'); //placeholder file path
-  //later, implement dialog for user to input file path.
-  //replace this file path with that later
-	videoProcess.then((video) => {
-    //store source video metadata
-    sourceVideoMetadata = video.metadata;
-	}, function(err) {
-		console.log('Error: ' + err);
-	});
-} catch(e) {
-	console.log(e.code);
-	console.log(e.msg);
-}
-
-ipcMain.on("sourceMetadataRequest", (event) => {
-  console.log("message received");
-  event.sender.send("sourceMetadataSend", sourceVideoMetadata);
-});
-*/
