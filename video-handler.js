f28541e1dfcbb69f42aa489c99e27c460f2cb8f5
HoWingYip <sploooosh884@gmail.com>
2018-12-01 22:46:44 +0800

    You know what? Just generate every thumbnail again for every import. Still storing lastsha512 of metadata for file identification purposes later on though.

--- video-handler.js
+++ video-handler.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const ffmpeg = require("fluent-ffmpeg");
+const hasha = require("hasha");
 const {ipcMain} = require("electron");
 const {dialog} = require("electron");
 
@@ -9,75 +10,68 @@ var ffmpegProcesses = new Array();
 
 //show import dialog on click of import area
 ipcMain.on("importAssets", (importedAssetsRequest) => {
-  try {
-    dialog.showOpenDialog({
-      title: "Import",
-      defaultPath: "~/",
-      buttonLabel: "Import",
-      filters: [
-        {name: "All Supported Files", extensions: ["mp4", "mov", "ogg", "webm", "m4v", "wav", "mp3", "webm", "aac", "flac", "m4a", "ogg", "oga", "opus", "png", "jpg", "jpeg", "bmp", "gif", "webp"]},
-        {name: "Video Files", extensions: ["mp4", "mov", "ogg", "webm", "m4v"]},
-        {name: "Audio Files", extensions: ["wav", "mp3", "webm", "aac", "flac", "m4a", "ogg", "oga", "opus"]},
-        {name: "Image Files", extensions: ["png", "jpg", "jpeg", "bmp", "gif", "webp"]}
-      ],
-      properties: ["openFile", "multiSelections"]
-    }, (files) => {
-      console.log(`Files selected: \n${files}\n\n`);
-      try {
-        //check that import was not cancelled
-        //to avoid throwing "files is not iterable"
-        if(files !== undefined) {
-          //add newly imported files to file list
-          for(var filename of files) {
-            //write details to object in array
-            importedFiles.push(
-              {filename: filename, thumbnail: ""}
-            );
-          }
-          console.log(importedFiles);
+  dialog.showOpenDialog({
+    title: "Import",
+    defaultPath: "~/",
+    buttonLabel: "Import",
+    filters: [
+      {name: "All Supported Files", extensions: ["mp4", "mov", "ogg", "webm", "m4v", "wav", "mp3", "webm", "aac", "flac", "m4a", "ogg", "oga", "opus", "png", "jpg", "jpeg", "bmp", "gif", "webp"]},
+      {name: "Video Files", extensions: ["mp4", "mov", "ogg", "webm", "m4v"]},
+      {name: "Audio Files", extensions: ["wav", "mp3", "webm", "aac", "flac", "m4a", "ogg", "oga", "opus"]},
+      {name: "Image Files", extensions: ["png", "jpg", "jpeg", "bmp", "gif", "webp"]}
+    ],
+    properties: ["openFile", "multiSelections"]
+  }, (files) => {
+    try {
+      //check that import was not cancelled
+      //to avoid throwing "files is not iterable"
+      if(files !== undefined) {
+        for(var fileNumber in files) {
+          //add files to list of imported files
+          importedFiles.push(
+            {filename: files[fileNumber], thumbnail: "", metadata: null, lastsha512: ""}
+          );
+          storeMetadataAndHash(fileNumber);
+          extractThumbnail(fileNumber);
         }
-      } catch(e) {
-        console.error(e);
+        //notify ipcRenderer of file import
+        importedAssetsRequest.sender.send("importedAssetsSend", importedFiles);
       }
-      console.log(`importedFiles array: \n${importedFiles}\n\n`);
-      //notify ipcRenderer of file import
-      importedAssetsRequest.sender.send("importedAssetsSend", importedFiles);
-      extractThumbnails();
-    });
-  } catch(e) {
-    console.error(e);
-  }
+    } catch(e) {
+      console.error(e);
+    }
+  });
 });
 
-function extractThumbnails() {
-  //create new ffmpeg instance for every video
-  //with path to video file
-  for(var fileNumber in importedFiles) {
-    //if thumbnail already exists, don't generate it again
-    //could bug out by showing old thumbnail if file was changed but still has same name
-    //CHANGE TO USE SHA512 OF FILE INSTEAD
-    if(!fs.existsSync("saved-frames-test/thumbnail-" + path.basename(importedFiles[fileNumber].filename) + ".png")) {
-      ffmpegProcesses[fileNumber] = new ffmpeg(importedFiles[fileNumber].filename);
-      ffmpegProcesses[fileNumber].on("filenames", (filenames) => {
-        //set thumbnail paths
-        for(var thumbnailNumber in filenames) {
-          importedFiles[thumbnailNumber].thumbnail = filenames[thumbnailNumber];
-          console.log(filenames[thumbnailNumber]);
-        }
-        console.log("Generating thumbnails: " + filenames.join(", "));
-      }).on("end", () => {
-        console.log("Thumbnails generated");
-      }).screenshots({
-        timestamps: [JSON.stringify(Math.random()) + "%"],
-        count: 1,
-        filename: "thumbnail-%f", //generate file with name "thumbnail-(filename)"
-        folder: "saved-frames-test/"
-      }).on("error", (err, stdout, stderr) => {
-        console.error(err);
-        if(stderr) console.error(`FFmpeg encountered an error:\n${stderr}\n\n`);
-        if(stdout) console.error(`FFmpeg output:\n${stdout}\n\n`);
-      });
-    }
-  }
+function storeMetadataAndHash(fileNumber) {
+  ffmpegProcesses[fileNumber] = new ffmpeg(importedFiles[fileNumber].filename);
+  ffmpegProcesses[fileNumber].ffprobe((err, metadata) => {
+    if(err) throw err;
+    importedFiles[fileNumber].metadata = metadata;
+    importedFiles[fileNumber].lastsha512 = hasha(JSON.stringify(metadata));
+    console.log(`Metadata for file #${fileNumber}`, importedFiles[fileNumber].metadata);
+  });
 }
 
+function extractThumbnail(fileNumber) {
+  //create new ffmpeg instance for every video
+  //with path to video file
+  ffmpegProcesses[fileNumber].on("filenames", (filename) => {
+    //DON'T FORGET - FILENAME IS AN ARRAY OF LENGTH 1
+    //(because this function runs one file at a time)
+    //set thumbnail paths
+    importedFiles[fileNumber].thumbnail = "saved-frames-test/" + filename[0];
+    console.log("Generating thumbnail: " + filename[0]);
+  }).on("end", () => {
+    console.log("Thumbnail generated");
+  }).screenshots({
+    timestamps: [JSON.stringify(Math.random()) + "%"],
+    count: 1,
+    filename: "thumbnail-%f", //generate file with name "thumbnail-(filename)"
+    folder: "saved-frames-test/"
+  }).on("error", (err, stdout, stderr) => {
+    console.error(err);
+    if(stderr) console.error(`FFmpeg encountered an error:\n${stderr}\n\n`);
+    if(stdout) console.error(`FFmpeg output:\n${stdout}\n\n`);
+  });
+}
