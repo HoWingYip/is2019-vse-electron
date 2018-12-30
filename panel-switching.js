a80d0254a31286346ac5df09a0e720552f278cbc
HoWingYip <sploooosh884@gmail.com>
2018-12-30 21:21:46 +0800

    Improved panel switcher script readability by caching element references

--- panel-switching.js
+++ panel-switching.js
@@ -1,35 +1,41 @@
+// init elements
+const sourcePanelContainer = document.getElementById("panel-container-source");
+const mediaPanelContainer = document.getElementById("panel-container-media");
+const sourceSwitcher = document.getElementById("switchSource");
+const mediaSwitcher = document.getElementById("switchMedia");
+
 // switch to source panel in div.video-panel
 function switchSource() {
   // display source video container
-  document.getElementById("panel-container-source").style.display = "inline-block";
+  sourcePanelContainer.style.display = "inline-block";
   // hide media browser
-  document.getElementById("panel-container-media").style.display = "none";
+  mediaPanelContainer.style.display = "none";
 
   // highlight source video tab by applying gradient
-  document.getElementById("switchSource").style.backgroundImage = "linear-gradient(rgb(75, 75, 75), rgb(95, 95, 95))";
+  sourceSwitcher.style.backgroundImage = "linear-gradient(rgb(75, 75, 75), rgb(95, 95, 95))";
   // brighten source video tab text colour
-  document.getElementById("switchSource").style.color = "rgb(209, 209, 209)";
+  sourceSwitcher.style.color = "rgb(209, 209, 209)";
 
   // un-highlight media browser tab by taking away gradient
-  document.getElementById("switchMedia").style.backgroundImage = "none";
+  mediaSwitcher.style.backgroundImage = "none";
   // darken media browser tab text colour
-  document.getElementById("switchMedia").style.color = "rgb(185, 185, 185)";
+  mediaSwitcher.getElementById("switchMedia").style.color = "rgb(185, 185, 185)";
 }
 
 // switch to media panel in div.video-panel
 function switchMedia() {
   // hide source video container
-  document.getElementById("panel-container-source").style.display = "none";
+  sourcePanelContainer.style.display = "none";
   // display media browser
-  document.getElementById("panel-container-media").style.display = "inline-block";
+  mediaPanelContainer.style.display = "inline-block";
 
   // un-highlight source video tab by taking away gradient
-  document.getElementById("switchSource").style.backgroundImage = "none";
+  sourceSwitcher.style.backgroundImage = "none";
   // darken source video tab text colour
-  document.getElementById("switchSource").style.color = "rgb(185, 185, 185)";
+  sourceSwitcher.style.color = "rgb(185, 185, 185)";
 
   // highlight media browser tab by applying gradient
-  document.getElementById("switchMedia").style.backgroundImage = "linear-gradient(rgb(75, 75, 75), rgb(95, 95, 95))";
+  mediaSwitcher.style.backgroundImage = "linear-gradient(rgb(75, 75, 75), rgb(95, 95, 95))";
   // brighten media browser tab text colour
-  document.getElementById("switchMedia").style.color = "rgb(209, 209, 209)";
+  mediaSwitcher.style.color = "rgb(209, 209, 209)";
 }
